d2fdff1fb4988e67113f12ac66651da8610e13b4
Abdul Shaik <sarfrazshaikz786@gmail.com>
2022-09-04 14:16:16 +0530

    added some more elements and styling

---
+++ Calculator/script.js
@@ -0,0 +1,22 @@
+let oneE = document.getElementById("btn-1")
+let twoE = document.getElementById("btn-2")
+let threeE = document.getElementById("btn-3")
+let fourE = document.getElementById("btn-4")
+let fiveE = document.getElementById("btn-5")
+let sixE = document.getElementById("btn-6")
+let sevenE = document.getElementById("btn-7")
+let eightE = document.getElementById("btn-8")
+let nineE = document.getElementById("btn-9")
+let zeroE = document.getElementById("btn-0")
+let equalE = document.getElementById("btn-ans")
+let pointE = document.getElementById("btn-decimal")
+let addE = document.getElementById("btn-add")
+let subE = document.getElementById("btn-sub")
+let mulE = document.getElementById("btn-mul")
+let divE = document.getElementById("btn-div")
+
+
+
+function input(){
+    
+}
