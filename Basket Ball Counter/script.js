d278f8e8ff965d1952abbe78fd2ee336c64c3a1d
Abdul Shaik <sarfrazshaikz786@gmail.com>
2022-09-09 18:37:57 +0530

    Made some changes in styling and adding the addition functionality

---
+++ Basket Ball Counter/script.js
@@ -0,0 +1,15 @@
+let homeScore = document.getElementsByClassName("home-score")
+let guestScore = document.getElementsByClassName("guest-score")
+
+console.log(homeScore);
+
+let guestElement; 
+
+
+function add(x,location){
+    if(location=='home'){
+        homeScore.textContent = '1';
+    }else if(location=='guest'){
+
+    }
+}
